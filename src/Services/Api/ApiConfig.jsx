14ee1d1994cb673c561d56c6de9a6f3a9ea902fd
Andre <andrweb876@gmail.com>
2024-09-13 11:13:00 -0300

    ...

--- src/Services/Api/ApiConfig.jsx
+++ src/Services/Api/ApiConfig.jsx
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const Api = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3001",
-=======
-  baseURL: "http://localhost:3001/",
->>>>>>> d719ab91c262dc383a8c9c517c3a7f9c2999b3d8
+  baseURL: "http://localhost:3001", // URL da API
   withCredentials: true,
 });
